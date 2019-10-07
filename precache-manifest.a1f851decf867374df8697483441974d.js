d209ec8df06147535f1ca4f81683979f99c0c595
IgorokNerovnyi1980 <i.nerovnyi@gmail.com>
2019-10-07 14:36:04 +0300

    Updates

---
+++ precache-manifest.a1f851decf867374df8697483441974d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91081fb3f7df59bfbede205cc8725703",
+    "url": "/test-game/index.html"
+  },
+  {
+    "revision": "7f49d08b03a2a6437e32",
+    "url": "/test-game/static/css/main.d412e047.chunk.css"
+  },
+  {
+    "revision": "10cb21f84267581d2e6f",
+    "url": "/test-game/static/js/2.09e23069.chunk.js"
+  },
+  {
+    "revision": "7f49d08b03a2a6437e32",
+    "url": "/test-game/static/js/main.14728cbd.chunk.js"
+  },
+  {
+    "revision": "3d20a7ac1395918b8608",
+    "url": "/test-game/static/js/runtime-main.5c7bc19c.js"
+  }
+]);
